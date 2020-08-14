bb67578b97fe9377579af114520b0260f357f4f7
Shabbir936 <69672927+Shabbir936@users.noreply.github.com>
2020-08-14 15:36:44 +0530

    Update Program.cs

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -11,7 +11,7 @@ namespace App1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi");
-            Console.WriteLine("first program");
+            Console.WriteLine("program");
             Console.ReadLine();
 
         }
